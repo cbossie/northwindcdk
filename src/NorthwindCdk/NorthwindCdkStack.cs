1c0f8104f575baff281e10bd181bc8a28d515a93
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2020-11-02 19:05:12 -0800

    Initial commit

---
+++ src/NorthwindCdk/NorthwindCdkStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace NorthwindCdk
+{
+    public class NorthwindCdkStack : Stack
+    {
+        internal NorthwindCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
